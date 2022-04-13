32636ffc5f002c1092bf35eb65b2ef7ece9c5d6b
yanrk <yanrkchina@163.com>
2022-04-13 19:24:36 +0800

    add setenv/getenv

--- inc/utility/utility.h
+++ inc/utility/utility.h
@@ -13,12 +13,18 @@
 #define GOOFER_UTILITY_H
 
 
+#include <list>
+#include <string>
 #include "common/common.h"
 
 NAMESPACE_GOOFER_BEGIN
 
 GOOFER_CXX_API(size_t) get_pid();
 GOOFER_CXX_API(size_t) get_tid();
+GOOFER_CXX_API(bool) get_env(const char * name, std::string & value);
+GOOFER_CXX_API(bool) set_env(const char * name, const std::string & value, bool overwrite = true);
+GOOFER_CXX_API(bool) get_env(const char * name, std::list<std::string> & values);
+GOOFER_CXX_API(bool) set_env(const char * name, const std::list<std::string> & values, bool overwrite = true);
 
 NAMESPACE_GOOFER_END
 
--- src/utility/utility.cpp
+++ src/utility/utility.cpp
@@ -16,7 +16,15 @@
     #include <pthread.h>
 #endif // _MSC_VER
 
+#include <cstdlib>
+#include "string/string.h"
 #include "utility/utility.h"
+
+#ifdef _MSC_VER
+    #define ENV_DELIMITER ";"
+#else
+    #define ENV_DELIMITER ":"
+#endif // _MSC_VER
 
 NAMESPACE_GOOFER_BEGIN
 
@@ -38,6 +46,58 @@ size_t get_tid()
 #else
     return (static_cast<size_t>(pthread_self()));
 #endif // _MSC_VER
+}
+
+bool get_env(const char * name, std::string & value)
+{
+    value.clear();
+    if (nullptr == name)
+    {
+        return (false);
+    }
+    const char * val = getenv(name);
+    if (nullptr == val)
+    {
+        return (false);
+    }
+    value = val;
+    return (true);
+}
+
+bool set_env(const char * name, const std::string & value, bool overwrite)
+{
+    if (nullptr == name)
+    {
+        return (false);
+    }
+#if _WIN32
+    if (!overwrite && nullptr != getenv(name))
+    {
+        return (true);
+    }
+    return (0 == _putenv_s(name, value.c_str()));
+#else
+    return (0 == setenv(name, value.c_str(), overwrite ? 1 : 0));
+#endif
+}
+
+bool get_env(const char * name, std::list<std::string> & values)
+{
+    values.clear();
+    std::string value;
+    if (!get_env(name, value))
+    {
+        return (false);
+    }
+    Goofer::goofer_split_piece(value, ENV_DELIMITER, true, true, values);
+    return (true);
+}
+
+bool set_env(const char * name, const std::list<std::string> & values, bool overwrite)
+{
+    std::string value;
+    Goofer::goofer_piece_together(values.begin(), values.end(), ENV_DELIMITER, value);
+    return (set_env(name, value, overwrite));
 }
 
 NAMESPACE_GOOFER_END
